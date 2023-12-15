822e1385eb7ea620b55b6ca7e204a4a77e8d554c
Msustersic <143512605+Msustersic@users.noreply.github.com>
2023-12-15 00:21:39 +0000

    14-12

---
+++ store_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+
+# Register your models here.
+
+from .models import Store
+
+@admin.register(Store)
+class StoresAdmin(ModelAdmin):
+    ...
